1841e64993aec09cbda4af15b37e41e187441496
tuanta <tuantran.kum@gmail.com>
2023-01-12 15:13:57 +0700

    3 backend - controller posts

--- routers/posts.js
+++ routers/posts.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import { getPosts } from '../controllers/posts.js';     // nhớ phải có .JS
 
 const posts = express.Router();
 
-posts.get('/', (req, res) => {
-    res.send('ROUTER - POSTS : success');
-})
+posts.get('/', getPosts)
 
 export default posts;
